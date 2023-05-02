06e4cf10af1f7dbd86a08803f0f7c94dd2766c7b
JIT4Teaching <131724513+JIT4Teaching@users.noreply.github.com>
2023-05-02 20:33:24 +0000

    Added color to output of the file content.

--- assign8_1v2.py
+++ assign8_1v2.py
@@ -1,3 +1,8 @@
+import os
+
+def clear_screen():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
 def get_user_details():
     name = input("Enter your name: ")
     street_address = input("Enter your street address: ")
@@ -9,21 +14,25 @@ def write_to_file(file_name, name, street_address, phone_number):
         f.write(f"{name}, {street_address}, {phone_number}\n")
 
 def read_file(file_name):
-    with open(file_name, 'r') as f:
-        print(f"Contents of {file_name}:")
+  red = "\033[31m"
+  reset = "\033[0m"
+    
+  with open(file_name, 'r') as f:
+        print(f"\nContents of file - {file_name}:\n")
         for line in f:
-            print(line.strip())
+            print(f"{red}{line.strip()}{reset}")
 
 def main():
     file_name = input("Enter the file name: ")
     while True:
+        clear_screen()
         name, street_address, phone_number = get_user_details()
         write_to_file(file_name, name, street_address, phone_number)
         read_file(file_name)
 
-        another_entry = input("Would you like to input another entry? (yes/no): ").lower()
+        another_entry = input("\nWould you like to input another entry? (yes/no): ").lower()
         if another_entry not in ('y', 'yes'):
-            print("Closing file and exiting program.")
+            print("\nClosing file and exiting program. Goodbye!")
             break
 
 if __name__ == "__main__":
